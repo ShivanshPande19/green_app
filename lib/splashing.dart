c097883e54a35e9892212609bd11adacc212f1a2
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2024-02-02 15:56:10 +0530

    splashed screen edited

--- lib/splashing.dart
+++ lib/splashing.dart
@@ -1,54 +1,82 @@
-// it will have the code of the splash screen
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:project_snu/main.dart';
+
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    Timer(const Duration(seconds: 2000),(){
-      Navigator.pushReplacement(context,
-      MaterialPageRoute(builder: (context)=>const MyHomePage()));
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _animationController.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MyHomePage()),
+      );
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Positioned(
-              left: 90.0,
-              top: 150.0,
-              child: Image.asset(
-                'images/image1.png',
-                height: 311,
-                width: 253,),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Image.asset(
-                  'images/bottom_image.png',
-                  width: 430,
-                  height: 328,
+      body: Stack(
+        children: [
+          Positioned(
+            left: 90,
+            top: 150,
+            child: Center(
+              child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: Hero(
+                  tag: 'imageHero',
+                  child: Image.asset(
+                    'images/image1.png',
+                    height: 311,
+                    width: 253,
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Image.asset(
+                'images/bottom_image.png',
+                width: 430,
+                height: 328,
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
